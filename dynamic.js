9b6b5b11348214065bdc9178a020edc575d92543
AhsanPhero2022e <whitehat2090@gmaile.com>
2023-03-04 23:32:00 +0600

    finished

--- dynamic.js
+++ dynamic.js
@@ -1,12 +1,27 @@
 const allData = () => {
   fetch("https://openapi.programming-hero.com/api/ai/tools")
     .then((res) => res.json())
-    .then((data) => showData(data.data.tools));
+    .then((data) => showData(data.data.tools.slice(0, 6)));
+
   toggleSpinner(true);
 };
 
 const showData = (singleData) => {
+  document.getElementById("btn-sort").addEventListener("click", function () {
+    const sortByDate = (a, b) => {
+      const dateA = new Date(a.published_in);
+      const dateB = new Date(b.published_in);
+
+      if (dateA < dateB) return 1;
+      else if (dateA > dateB) return -1;
+      return 0;
+    };
+    const sortData = singleData.sort(sortByDate);
+    showData(sortData);
+  });
+
   const parants = document.getElementById("parants-div");
+  parants.innerHTML = "";
   singleData.forEach((totalData) => {
     const div = document.createElement("div");
 
@@ -67,7 +82,7 @@ const fetchUniversId = async (id) => {
 };
 
 const displayUniverDetails = (univer) => {
-  console.log(univer.features[1].feature_name);
+  // console.log(univer);
   document.getElementById("modal-description").innerHTML = `
          
 
@@ -96,29 +111,47 @@ const displayUniverDetails = (univer) => {
                 </div>
                 </div>
                 
+
+                 <div class="flex justify-between">
+    <div>
+    <h3 class="font-semibold">
+    Features
+    </h3>
+    <ul>
+    <li><small>1. ${univer.features[1].feature_name}</small></li>
+    <li><small>2. ${univer.features[2].feature_name}</small></li>
+    <li><small>3. ${univer.features[3].feature_name}</small></li>
+    </ul>
+    </div> 
+
+    <div>
+  <h3 class="font-semibold">
+  Integrations
+  </h3>
+  <ul>
+  <li><small>1. ${
+    univer.features[1].feature_name
+      ? univer.features[1].feature_name
+      : "No data Found"
+  }</small></li>
+    <li><small>2. ${
+      univer.features[2].feature_name
+        ? univer.features[2].feature_name
+        : "No data Found"
+    }</small></li>
+    <li><small>3. ${
+      univer.features[2].feature_name
+        ? univer.features[2].feature_name
+        : "No data Found"
+    }</small></li>
+    </ul>
+    </div>
+    </div>
+  </div>
+</div>
+                
               </div>
             </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -127,9 +160,15 @@ const displayUniverDetails = (univer) => {
 
             
 
-            <div class="card w-96 bg-base-100 shadow-xl">
+            <div class="p-4 card w-96 bg-base-100 shadow-xl">
               
-              <figure><img src="${univer.image_link}" alt="" /></figure>
+              <figure><img class="relative" src="${
+                univer.image_link[0]
+              }" alt="" />
+              <div id="hidden" class="hidden absolute top-7 right-7">${
+                univer.accuracy.score
+              }% accuracy</div>
+              </figure>
               <div class="card-body">
             <p class="font-semibold">${
               univer.input_output_examples[0].input
@@ -137,12 +176,21 @@ const displayUniverDetails = (univer) => {
                 : "No! Not Yet! Take a break!!!"
             }</p>
             <p>${
-              univer.input_output_examples
-                ? univer.input_output_examples[0].output
+              univer.input_output_examples[1].output
+                ? univer.input_output_examples[1].output
                 : "No! Not Yet! Take a break!!!"
             }</p>
               </div>
             </div>
   `;
+
+  if (univer.accuracy.score) {
+    document.getElementById("hidden").classList.remove("hidden");
+  }
 };
+
+// document
+//   .getElementById("btn-show-all")
+//   .addEventListener("click", function () {});
+
 allData();
