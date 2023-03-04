2088689e5c37aa3c84470fcd8184a2bd39fc23e2
AhsanPhero2022e <whitehat2090@gmaile.com>
2023-03-04 15:44:57 +0600

    modal done

--- dynamic.js
+++ dynamic.js
@@ -1,14 +1,13 @@
 const allData = () => {
   fetch("https://openapi.programming-hero.com/api/ai/tools")
     .then((res) => res.json())
-    .then((data) => showData(data.data));
+    .then((data) => showData(data.data.tools));
   toggleSpinner(true);
 };
 
 const showData = (singleData) => {
-  console.log(singleData);
   const parants = document.getElementById("parants-div");
-  singleData.tools.forEach((totalData) => {
+  singleData.forEach((totalData) => {
     const div = document.createElement("div");
 
     div.classList.add("col");
@@ -19,9 +18,9 @@ const showData = (singleData) => {
               <h5 class="card-title">${totalData.name}</h5>
               
              <ol>
-              <li>${totalData.features[0]}</li>
-              <li>${totalData.features[1]}</li>
-              <li>${totalData.features[3]}</li>
+              <li>1. ${totalData.features[0]}</li>
+              <li>2. ${totalData.features[1]}</li>
+              <li>3. ${totalData.features[3]}</li>
               </ol>
               
             
@@ -34,7 +33,7 @@ const showData = (singleData) => {
             ${totalData.published_in}</p>
         </div>
         <div>
-        <label onclick="modalData('${totalData.id}')" for="my-modal-5" class="btn btn-outline btn-info btn-circle btn-xs">
+        <label for="universId" onclick="fetchUniversId('${totalData.id}')" for="my-modal-5" class="btn btn-outline btn-info btn-circle btn-xs">
         <i class="fa-solid fa-angles-right"></i>
         </label>
         </div>
@@ -59,27 +58,70 @@ const toggleSpinner = (isLoading) => {
 };
 
 // Modal start
-const modalData = (tools_id) => {
-  const url = `https://openapi.programming-hero.com/api/ai/tool/${tools_id}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => modalInformation(data.data.id));
+const fetchUniversId = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+  //   console.log(singleData);
+  const res = await fetch(url);
+  const data = await res.json();
+  displayUniverDetails(data.data);
 };
 
-const modalInformation = (modalInfo) => {
-  console.log(modalInfo);
+const displayUniverDetails = (univer) => {
+  console.log(univer);
+  document.getElementById("modal-description").innerHTML = `
+         
 
-  const parant = document.getElementById("modal-info");
-  const firstDiv = document.createElement("div");
-  firstDiv.innerHTML = `
-  <h2>${modalInfo.image}</h2>
+            <div class="card w-96 bg-base-100 shadow-xl">
+              <div class="card-body">
+                <h2 class="card-title">${univer.description}</h2>
+                
+              </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+            <div class="card w-96 bg-base-100 shadow-xl">
+              
+              <figure><img src="${univer.image_link}" alt="" /></figure>
+              <div class="card-body">
+            <p>${
+              univer.input_output_examples[0].input
+                ? univer.input_output_examples[0].input
+                : "No! Not Yet! Take a break!!!"
+            }</p>
+            <p>${
+              univer.input_output_examples
+                ? univer.input_output_examples[0].output
+                : "No! Not Yet! Take a break!!!"
+            }</p>
+              </div>
+            </div>
   `;
-  parant.appendChild(firstDiv);
-  const imgParant = document.getElementById("modal-img");
-  const div = document.createElement("div");
-  div.innerHTML = `
-  <img class="img-fluid" src="${modalInfo.image}" alt="">
-  `;
-  imgParant.appendChild(div);
 };
 allData();
